e6cf53253324e960e0f263292c09901bba28ab4e
“geniuscodemycombr” <geniuscodemy.com.br@gmail.com>
2026-02-03 11:14:49 -0300

    Ajustes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: "My Site",
+  title: "Mondaha",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://docs.mondaha.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -75,7 +75,7 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: "My Site",
+      title: "Mondaha",
       logo: {
         alt: "My Site Logo",
         src: "img/logo-mondaha.svg",
